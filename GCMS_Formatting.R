686ce36ec63a95c05111e5eca6a7a2d965768b0e
Max <max.tyree13@gmail.com>
2025-05-18 21:44:21 -0400

    Finished making the line graph generator work with all types of data (HILIC, GCMS, and LCMS)

--- GCMS_Formatting.R
+++ GCMS_Formatting.R
@@ -1,49 +1,29 @@
+library(tidyr)
 library(ggplot2)
-library(tidyverse)
 
-data <- read.csv("GCMS_Data.csv")
-data <- as_tibble(data)
-#create a long format table of time values to append later
-time_col <- data
-#define each column as a time point in new df
-colnames(time_col) <- time_col[1, ]
-#delete all columns and rows that aren't times
-time_col <- time_col[-1, -1]
-#convert to long format
-time_col <- time_col |>
-  pivot_longer(
-    cols = everything(),
-    names_to = "log2_hours",
-    values_to = "values"
-  )
-#create a long format data table not yet including time
-combined_data <- data[-1, ] |>
-  pivot_longer(
-    cols = -Compound,
-    names_to = "yeast",
-    values_to = "rel_abundance"
-  )
-#add time to the data table from col 1 of the time_col df
-combined_data <- combined_data |>
-  mutate(log2_hrs = pull(time_col, 1))
-#if the data is from lcms data it will need this if to transform yeast names
-if(str_length(combined_data$yeast[[1]]) > 4){
-  combined_data <- combined_data |>
-    mutate(yeast = tolower(substr(yeast, 19, 21)))
-  #if the data is from gcms data it needs this else condition
-} else {
-  combined_data <- combined_data |>
-    mutate(yeast = substr(yeast, 1, 3))
-}
-
-combined_data <- combined_data |>
-  rename(hrs = log2_hrs) |>
-  mutate(hrs = 2^as.numeric(hrs))
-
-
-
-combined_data <- combined_data[, c("Compound", "hrs", "yeast", "rel_abundance")]
-write.csv(combined_data, file = "GCMS_Data.csv", append = FALSE, quote = FALSE, row.names = FALSE)
-
-
-
+df <- read.csv("GCMS_Data_Unformatted.csv", na.strings = "", header = FALSE)
+df <- as_tibble(df)
+df1 <- df[-1, ]
+df2 <- df[-2, ]
+colnames(df1) <- df1[1, ]
+colnames(df2) <- df2[1, ]
+df1 <- df1[-1, ]
+df2 <- df2[-1, ]
+colnames(df1)[1] <- "Compound"
+colnames(df2)[1] <- "Compound"
+df1_longer <- df1 |>
+  pivot_longer(cols = -Compound, names_to = "yeast", values_to = "rel_abundance")
+df2_longer <- df2 |>
+  pivot_longer(cols = -Compound, names_to = "hrs", values_to = "rel abundance")
+combined_data <- df1_longer |>
+  mutate(hrs = pull(df2_longer, 2))
+combined_data <- as.data.frame(combined_data)
+combined_data$yeast <- gsub("ExtrF", "sak", combined_data$yeast)
+combined_data$yeast <- gsub("Eng", "eng", combined_data$yeast)
+combined_data$yeast <- gsub("Cal", "cal", combined_data$yeast)
+combined_data$yeast <- gsub("Bel", "bel", combined_data$yeast)
+combined_data$yeast <- gsub("Pils", "pil", combined_data$yeast)
+combined_data$yeast <- gsub("pre-ferm", "po2", combined_data$yeast)
+combined_data <- combined_data[, c(1,4,2,3)]
+write.csv(combined_data, file = "GCMS_Data_Transformed.csv", row.names = FALSE)
+gcms_data <- read.csv("GCMS_Data_Transformed.csv", sep = ",")
--- Line Graphs.R
+++ Line Graphs.R
@@ -1,74 +1,91 @@
 library(ggplot2)
 library(tidyverse)
 library(outliers)
+`%||%` <- function(a, b) if (!is.null(a)) a else b
 
-data <- read.csv("GCMS_Data.csv")
+data <- read.csv("GCMS_Data_Transformed.csv")
 data[[4]][data[[4]] == 0] <- NA
 data <- data |>
   group_by(Compound, yeast, hrs) |>
-  filter(!any(is.na(across(rel_abundance)))) |>
+  filter(!all(is.na(across(rel_abundance)))) |>
   ungroup()
 
 #average the triplicate data and convert to non log rel_abundance
-  if(class(data[[1]]) == "character") {
+if(class(data[[1]]) == "character") {
   avg_data <- data |> 
     group_by(Compound, yeast, hrs) |>
-    mutate(rel_abundance = (mean(as.numeric(rel_abundance)))) |>
+    mutate(rel_abundance = (mean(as.numeric(rel_abundance), na.rm = TRUE))) |>
     distinct() |>
     ungroup()
-  } else {
+} else {
   avg_data <- data |> 
     group_by(Compound, yeast, hrs) |>
     mutate(rel_abundance = 2^(mean(as.numeric(rel_abundance)))) |>
     distinct() |>
     ungroup()
-  }
+}
 
 #create subset of the averaged data that just includes your compound of interest
-  cmpd_avg_data <- avg_data |> 
-    filter(
-    str_detect(Compound, "^ethyl acetate")
-    )
-if(class(data[[1]]) == "character" & data[[2,1]] != "ethyl acetate") {
+cmpd_avg_data <- avg_data |> 
+  filter(
+    #    Compound == "2-methylbutyraldehyde")
+    str_detect(Compound, "^2 & 3-methyl-1-butanol"))
+if(class(data[[1]]) == "character" & data[[2,1]] != "2-methyl-furan ") {
   ref_value <- cmpd_avg_data |>
-  group_by(rel_abundance) |>
-  mutate(dup_count = n()) |>
-  filter(dup_count == 5) |>
-  ungroup() |>
-  slice_max(order_by = hrs, n = 1)
-ref_value <- as.double(ref_value[[1,4]])
+    group_by(rel_abundance) |>
+    mutate(dup_count = n()) |>
+    filter(dup_count == 5) |>
+    ungroup() |>
+    slice_max(order_by = hrs, n = 1)
+  ref_value <- as.double(ref_value[[1,4]])
 } else {
   ref_value <- cmpd_avg_data |>
     filter(yeast == "po2")
-  ref_value <- as.double(ref_value[[1,4]])
+  if(nrow(ref_value) > 0) {
+    ref_value <- as.double(ref_value[which.max(ref_value[[2]]), 4])
+  } else {
+    ref_value <- 1
+  }
 }
 
 cmpd_avg_data <- cmpd_avg_data |>
   mutate(rel_abundance = rel_abundance/ref_value)
-  
+
 
 #define label positions for series
 xpos <- 6
 ypos <- 2
-offset <- 0.5
+offset <- 0.3
+
+cmpd_avg_data$yeast <- factor(cmpd_avg_data$yeast)
 
 #plot data
-ggplot(cmpd_avg_data, aes(x = log(hrs), y = rel_abundance, color = yeast, group = yeast)) +
-    geom_line(linewidth = 1, show.legend = FALSE) +
-    geom_point(show.legend = FALSE) +
-    labs(title = "Phenylpyruvic acid") +
-    xlab("time") + 
-    ylab("Relative Abundance") +
-    theme_classic(base_size = 24) +
-    theme(
-      axis.text = element_text(color = "black"),
-      plot.margin = margin(5.5, 60, 5.5, 5.5)
-    ) +
-    coord_cartesian(clip = "off")+
-#label maker
-  annotate("text", x = xpos, y = ypos+offset*4, label = "Belgian", color = "#F8766D", hjust = 0, size = 6) +
-  annotate("text", x = xpos, y = ypos+offset*3, label = "California", color = "#A3A500", hjust = 0, size = 6) +
-  annotate("text", x = xpos, y = ypos+offset*2, label = "English", color = "#00BF7D", hjust = 0, size = 6) +
-  annotate("text", x = xpos, y = ypos+offset, label = "Pilsner", color = "#00B0F6", hjust = 0, size = 6) +
-  annotate("text", x = xpos, y = ypos, label = "Sake", color = "#E76BF3", hjust = 0, size = 6)
-  
+my_plot <- ggplot(cmpd_avg_data, aes(x = log(as.numeric(hrs)), y = rel_abundance, color = yeast, group = yeast)) +
+  geom_line(linewidth = 1, show.legend = FALSE) +
+  geom_point(show.legend = FALSE) +
+  labs(title = "2 & 3-methyl-1-butanol") +
+  xlab("time") + 
+  ylab("Relative Abundance") +
+  theme_classic(base_size = 24) +
+  theme(
+    axis.text = element_text(color = "black"),
+    plot.margin = margin(5.5, 60, 5.5, 5.5)
+  ) +
+  coord_cartesian(clip = "off")
+
+plot_data <- ggplot_build(my_plot)
+group_colors <- plot_data$data[[1]] |>
+  select(group, colour) |>
+  distinct()
+yeast_levels <- levels(cmpd_avg_data$yeast)
+yeast_colors <- setNames(group_colors$colour, yeast_levels)
+
+my_plot +
+  #label maker
+  annotate("text", x = xpos, y = ypos+offset*5, label = "Pre O2", color = as.character(yeast_colors["po2"] %||% "#AAAAAA"), hjust = 0, size = 6) +
+  annotate("text", x = xpos, y = ypos+offset*4, label = "California", color = as.character(yeast_colors["cal"] %||% "#AAAAAA"), hjust = 0, size = 6) +
+  annotate("text", x = xpos, y = ypos+offset*3, label = "Belgium", color = as.character(yeast_colors["bel"] %||% "#AAAAAA"), hjust = 0, size = 6) +
+  annotate("text", x = xpos, y = ypos+offset*2, label = "English", color = as.character(yeast_colors["eng"] %||% "#AAAAAA"), hjust = 0, size = 6) +
+  annotate("text", x = xpos, y = ypos+offset, label = "Pilsner", color = as.character(yeast_colors["pil"] %||% "#AAAAAA"), hjust = 0, size = 6) +
+  annotate("text", x = xpos, y = ypos, label = "Sake", color = as.character(yeast_colors["sak"] %||% "#AAAAAA"), hjust = 0, size = 6)
+
